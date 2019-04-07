3b1c074aa2c9b0b7b68ba0dc22e1cb7b8fb6062c
Bartosz Stasik <bartolus99@gmail.com>
2019-04-07 13:07:34 +0100

    Add comments and improve readability

--- got-genealogy/src/main/java/com/got/genealogy/core/graph/Graph.java
+++ got-genealogy/src/main/java/com/got/genealogy/core/graph/Graph.java
@@ -18,14 +18,17 @@ public class Graph<Vert extends Vertex> {
     }
 
     public void addEdge(Vert vertex1, Vertex vertex2) {
+        // Get index numbers of vertices
         int fromVertex = vertices.get(vertex1);
         int toVertex = vertices.get(vertex2);
 
+        // Add outgoing edge
         matrix.get(fromVertex)
                 .get(toVertex)
                 .setWeight(1);
 
         if (!directed) {
+            // Add incoming edge
             matrix.get(toVertex)
                     .get(fromVertex)
                     .setWeight(1);
@@ -33,15 +36,19 @@ public class Graph<Vert extends Vertex> {
     }
 
     public void addVertex(Vert vertex) {
+        // Add vertex with new index
         vertices.put(vertex, vertices.size());
         int newIndex = matrix.size();
 
+        // Add new column to existing rows
         for (List<Weight<Integer>> row : matrix) {
             row.add(new Weight<>(0));
         }
 
+        // Add new empty row
         matrix.add(new ArrayList<>());
 
+        // Fill new row
         for (int i = 0; i <= newIndex; i++) {
             matrix.get(newIndex)
                     .add(new Weight<>(0));
@@ -65,16 +72,22 @@ public class Graph<Vert extends Vertex> {
     }
 
     public void printGraph() {
-        for (int i = 0; i < matrix.size(); i++) {
+        int size = matrix.size();
+        for (int i = 0; i < size; i++) {
+            // HashMap isn't ordered, so
+            // get correct vertex label
+            // and print with space.
             for (Map.Entry<Vert, Integer> vertex : vertices.entrySet()) {
                 if (i == vertex.getValue()) {
                     System.out.print(vertex.getKey().getLabel() + "  ");
                 }
             }
-            for (int j = 0; j < matrix.get(i).size(); j++) {
-                String printValue;
-                printValue = j == matrix.get(i).size() - 1 ? "" : ", ";
-                System.out.print(matrix.get(i).get(j).getWeight() + printValue);
+            // Print weights for vertices,
+            // for this row.
+            for (int j = 0; j < size; j++) {
+                String printValue = (j != size-1) ? ", " : "";
+                int weight = matrix.get(i).get(j).getWeight();
+                System.out.print(weight + printValue);
             }
             System.out.println();
         }
