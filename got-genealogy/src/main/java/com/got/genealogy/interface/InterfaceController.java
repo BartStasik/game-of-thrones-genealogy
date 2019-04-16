7c7af63fd607ba16eb9380fb9063150d4bdb448b
ashleighmm <ashleigh@hoist.co.za>
2019-04-16 23:23:26 +0100

    Added Id's to elements in fxmls

--- got-genealogy/src/main/java/com/got/genealogy/interface/InterfaceController.java
+++ got-genealogy/src/main/java/com/got/genealogy/interface/InterfaceController.java
@@ -1,14 +1,63 @@
-package interface;
- 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
  
-public class FXMLExampleController {
-    @FXML private Text actiontarget;
+public class InterfaceController {
+    @FXML
+    private Text character;
+
+    @FXML
+    private Button closeWindow;
+
+    @FXML
+    private Text house;
+
+    @FXML
+    private Text gender;
+
+    @FXML
+    private Text lifeStatus;
+
+    @FXML
+    private Text dob;
     
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        //actiontarget.setText("Sign in button pressed");
+    @FXML
+    private Text characterOne;
+
+    @FXML
+    private Text characterTwo;
+
+    @FXML
+    private Text actionWord;
+
+    @FXML
+    private Button closeWindow1;
+    
+    
+    
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        /*Window owner = submitButton.getScene().getWindow();x
+        if(nameField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your name");
+            return;
+        }
+        if(emailField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your email id");
+            return;
+        }
+        if(passwordField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter a password");
+            return;
+        }
+
+        AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
+                "Welcome " + nameField.getText());*/
     }
 
 }
