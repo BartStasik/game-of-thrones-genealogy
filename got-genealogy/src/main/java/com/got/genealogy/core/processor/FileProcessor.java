2e13e6f82bc05904aef4a2de6513d11c9b87c011
unknown <ahm3diqbal@gmail.com>
2019-04-19 12:45:10 +0100

    Create `GenealogyProcessor` class and implement sort method in `FileProcessor`

--- got-genealogy/src/main/java/com/got/genealogy/core/processor/FileProcessor.java
+++ got-genealogy/src/main/java/com/got/genealogy/core/processor/FileProcessor.java
@@ -1,126 +1,67 @@
 package com.got.genealogy.core.processor;
 
-import com.got.genealogy.core.family.person.Gender;
+import com.got.genealogy.core.family.FamilyTree;
 import com.got.genealogy.core.family.person.Person;
 import com.got.genealogy.core.family.person.Relation;
-import com.got.genealogy.core.family.FamilyTree;
 import com.got.genealogy.core.graph.collection.AdjacencyList;
 import com.got.genealogy.core.graph.property.WeightedVertex;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-
-import static com.got.genealogy.core.family.person.Gender.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FileProcessor {
 
-    private FamilyTree family = new FamilyTree();
-
-    public boolean loadFile(String absolutePath) {
+    public String[][] loadFile(String absolutePath) {
 
         try {
             BufferedReader br = new BufferedReader(
                     new FileReader(absolutePath));
-            String[] values;
+            List<String[]> trimmedFile = new ArrayList<>();
+            String[] words;
             String line;
 
             while ((line = br.readLine()) != null) {
-                values = line.split(", ");
+                words = line.split(",");
 
-                for (String s : values) {
-                    s.trim();
-
+                for (int i =0; i<words.length; i++) {
+                    words[i] = words[i].replaceAll("\\s{2,}", " ").trim();
                 }
+                trimmedFile.add(words);
 
-                Gender gender;
-
-                switch (values[1].toUpperCase()) {
-                    case "MALE":
-                    case "FATHER":
-                    case "MAN":
-                        gender = MALE;
-                        break;
-
-                    case "WOMAN":
-                    case "MOTHER":
-                    case "FEMALE":
-                        gender = MALE;
-
-                    default:
-                        gender = UNSPECIFIED;
-
-                }
-
-
-//                if (values[1].toUpperCase() == "MALE" || values[1].toUpperCase() == "FATHER") {
-//                    gender = MALE;
-//                } else if (values[1].toUpperCase() == "WOMAN" || values[1].toUpperCase() == "MOTHER") {
-//                    gender = FEMALE;
-//                } else{
-//                    gender = UNSPECIFIED;
-//                }
-
-                switch (values.length) {
-                    case 1:
-                        if (family.getPerson(values[0]) == null) {
-                            family.addPerson(values[0]);
-                        }
-                        break;
-                    case 2:
-                        if (family.getPerson(values[0]) == null) {
-                            family.addPerson(values[0], gender);
-                        }
-
-                        break;
-                    case 3:
-                        if (family.getPerson(values[0]) == null) {
-                            family.addPerson(values[0], gender);
-                        }
-
-
-                        if (family.getPerson(values[2]) == null) {
-                            family.addPerson(values[2], gender);
-                        }
-
-                        family.addRelation(values[0], values[2], new Relation(values[1]));
-                        break;
-                    default:
-                        System.out.println("INCORRECT FILE FORMAT");
-                        return false;
-                }
             }
+            String[][] cleanFile = new String[trimmedFile.size()][];
 
+            return trimmedFile.toArray(cleanFile);
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
-        return true;
     }
 
-    public boolean exportFile(String file) {
+    public boolean exportGVFile(String absolutePath, FamilyTree family) {
 
         PrintWriter writer;
         try {
-
-            writer = new PrintWriter(file+ ".gv");
+            writer = new PrintWriter(absolutePath + ".gv");
 
         } catch (Exception e) {
             e.printStackTrace();
-
             return false;
         }
 
-        writer.println("digraph " + file + "{ rankdir=LR;\n size =\"8,5\"");
+        writer.println("digraph " + absolutePath + "{ rankdir=LR;\n size =\"8,5\"");
         writer.println("node [shape=circle] [color=black];");
-
         String arc = " -> ";
 
         AdjacencyList<Person, Relation> list = family.adjacencyListWeighted();
         list.getList().forEach((k, v) -> {
             System.out.print(k.getLabel() + " : ");
-
 
             for (WeightedVertex<Person, Relation> person : v) {
                 System.out.print(person.getKey().getLabel() + ", ");
@@ -131,19 +72,28 @@ public class FileProcessor {
                 writer.print("\"");
                 writer.print(person.getKey().getLabel());
                 writer.print("\"");
-                writer.println(" [ label = \"" + list.getWeightedVertex(k,person.getKey()).getValue().getLabel() + "\"];");
+                writer.println(" [ label = \"" + list.getWeightedVertex(k, person.getKey()).getValue().getLabel() + "\"];");
             }
             System.out.println();
         });
-
 
         writer.println("}");
         writer.close();
         return true;
     }
 
+    public void sortPeople(FamilyTree family){
+        ArrayList<Person> sortedPeople = new ArrayList<>(family.getVertices().keySet());
+        Collections.sort(sortedPeople);
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("Sorted People.txt");
+            sortedPeople.forEach((e) -> System.out.println(e.getLabel()));
+            sortedPeople.forEach((e) -> writer.println(e.getLabel()));
+            writer.close();
 
-    public FamilyTree getFamily() {
-        return family;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
