a7876ee0d667f265f3a858cfabd58ea501d32fa1
ashleighmm <ashleigh@hoist.co.za>
2019-04-22 12:32:44 +0100

    Family relationship form displaying method results

--- got-genealogy/src/main/java/com/got/genealogy/userinterface/FXMLController.java
+++ got-genealogy/src/main/java/com/got/genealogy/userinterface/FXMLController.java
@@ -10,7 +10,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-	 
+import javafx.scene.control.Label;
+
+import static com.got.genealogy.core.processor.Genealogy.findRelationship;
+
 public class FXMLController {
 
     @FXML
@@ -26,22 +29,57 @@ public class FXMLController {
     private TextField person2;
 
     @FXML
-    private TextField dispField;
+    private Label dispField;
 
     @FXML
     private Button submitBtn;
 
     @FXML
-    private Button submitHistory;
+    private Button clearBtn;
+
+
 
     @FXML
     void findRelation(ActionEvent event) {
         String char1 = person1.getText();
         String char2 = person2.getText();
-        findRelationship(char1, char2);
 
-        dispField.setText(char1);
+        //Find relation
+        String[] relationship = findRelationship(char1, char2, "Stark");
+        //System.out.println(relationship);
+
+        for( int i = 0; i < relationship.length - 1; i++)
+        {
+            String element = relationship[i];
+            String nextElement = relationship[i+1];
+            dispField.setText(element + " and " + nextElement);
+        }
+
+//
+//        String current = relationship[i];
+//        if (i != relationship.length - 1) {
+//            String next = relationship[i+1];
+//            dispField.setText(next);
+//        }
+
+//        for(int i = 0; i < relationship.length; i++){
+//
+//            String element = relationship[i];
+//            dispField.setText(element);
+//            //add labels[i] to the appropriate Container
+//
+//        }
     }
+
+    @FXML
+    void clearFields(ActionEvent event) {
+        person1.clear();
+        person2.clear();
+        dispField.setText("");
+
+    }
+
+
 
     private Scene secondScene;
 
@@ -83,7 +121,7 @@ public class FXMLController {
         assert person1 != null : "fx:id=\"person1\" was not injected: check your FXML file 'main.fxml'.";
         assert person2 != null : "fx:id=\"person2\" was not injected: check your FXML file 'main.fxml'.";
         assert submitBtn != null : "fx:id=\"submitBtn\" was not injected: check your FXML file 'main.fxml'.";
-        assert submitHistory != null : "fx:id=\"submitHistory\" was not injected: check your FXML file 'main.fxml'.";
+
 
     }
 }
