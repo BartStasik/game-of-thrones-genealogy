db3d9ebc5e74f1ac2eac159d6405545d1ae2184d
JoshuaButton <sg.proveyourworth@gmail.com>
2019-04-23 14:42:23 +0100

    Added apache fileutil to maven for file to string and added filechooser for file paths add start

--- got-genealogy/src/main/java/com/got/genealogy/MainLoader.java
+++ got-genealogy/src/main/java/com/got/genealogy/MainLoader.java
@@ -8,8 +8,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import static com.got.genealogy.core.processor.Genealogy.loadRelation;
-import static com.got.genealogy.core.processor.Genealogy.loadPersonDetails;
+import static com.got.genealogy.core.processor.Genealogy.loadRelationsFile;
+import static com.got.genealogy.core.processor.Genealogy.loadPersonDetailsFile;
+import javafx.stage.FileChooser;
+import org.apache.commons.io.FileUtils;
 
 public class MainLoader extends Application {
 
@@ -27,7 +29,7 @@ public class MainLoader extends Application {
 
         AnchorPane interfacePane = interfaceLoader.load();
         VBox mainPane = mainLoader.load();
-        AnchorPane popupPane = popupLoader.load();
+        //AnchorPane popupPane = popupLoader.load();
 
         Scene firstScene = new Scene(interfacePane);
         Scene secondScene = new Scene(mainPane);
@@ -45,13 +47,21 @@ public class MainLoader extends Application {
         //FXMLController secondPaneController = (FXMLController) mainLoader.getController();
         //secondPaneController.setFirstScene(firstScene);
         
-//        FXMLController popupPaneController = (FXMLController) popupLoader.getController();
-//        popupPaneController.setPopupScene(popupScene);
-
-
-        //Get the test files from resources in order for laod methods etc in FXMLController to work
-        loadRelation("/Users/ashmac/GitHub/game-of-thrones-genealogy/got-genealogy/src/main/resources/InputFile.txt", "Stark");
-        loadPersonDetails("/Users/ashmac/GitHub/game-of-thrones-genealogy/got-genealogy/src/main/resources/PersonDetailsTestFile.txt", "Stark");
+        //FXMLController popupPaneController = (FXMLController) popupLoader.getController();
+        //popupPaneController.setPopupScene(popupScene);
+        
+        
+        //TemporaryDialogs for opening relation and details files
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Relations File");
+        loadRelationsFile(FileUtils.readFileToString(fileChooser.showOpenDialog(primaryStage)),"Stark");
+        
+        fileChooser.setTitle("Open Relations File");
+        loadPersonDetailsFile(FileUtils.readFileToString(fileChooser.showOpenDialog(primaryStage)),"Stark");
+        
+        //Get the test files from resources in order for load methods etc in FXMLController to work
+        //loadRelationsFile("/Users/ashmac/GitHub/game-of-thrones-genealogy/got-genealogy/src/main/resources/InputFile.txt", "Stark");
+        //loadPersonDetailsFile("/Users/ashmac/GitHub/game-of-thrones-genealogy/got-genealogy/src/main/resources/PersonDetailsTestFile.txt", "Stark");
         
     }
     
